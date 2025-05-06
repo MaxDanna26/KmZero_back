64cf154780089ef553d827b8d95cc7cdab61bd8c
MaxDanna26 <maximilianodanna@hotmail.com>
2025-05-06 15:58:13 +0200

    Merging in main with Max

--- src/application/middlewares/index.js
+++ src/application/middlewares/index.js
@@ -3,7 +3,7 @@ import cors from 'cors';
 import withSockets from './with-sockets';
 
 const Middlewares = (app, io) => {
-    app.use(express.urlencoded({extended: true}));
+    app.use(express.urlencoded({ extended: true }));
     app.use(express.json());
     app.use(withSockets(io));
     app.use(cors()) // Use this after the variable declaration
